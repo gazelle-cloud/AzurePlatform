c797d6f0bf75390ecb5128e66644375e74ccfb80
Mantas <your.email@example.com>
2024-08-15 22:44:30 +0200

    v7

--- .github/pwsh/move-subscriptions.ps1
+++ .github/pwsh/move-subscriptions.ps1
@@ -1,24 +1,23 @@
-function move-subscription {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$managementGroup,
-        [Parameter(Mandatory = $true)]
-        [string]$managementSubscriptionId
-    )
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$managementGroup,
+    [Parameter(Mandatory = $true)]
+    [string]$managementSubscriptionId
+)
     
-    Select-AzSubscription -Subscription $managementSubscriptionId
+Select-AzSubscription -Subscription $managementSubscriptionId
 
-    $query = "resourcecontainers
+$query = "resourcecontainers
       | where type == 'microsoft.resources/subscriptions'
       | where properties['managementGroupAncestorsChain'] contains '$managementGroup'
       | project subscriptionId"
 
-    $subscriptions = Search-AzGraph -Query $query
-    $subscriptions.Count
+$subscriptions = Search-AzGraph -Query $query
+$subscriptions.Count
     
-    foreach ($item in $subscriptions) {
-        $id = [string]$item.subscriptionId
-        New-AzManagementGroupSubscription -GroupName $managementGroup -SubscriptionId $id -WhatIf
-        Write-Output "----------------"
-    }
+foreach ($item in $subscriptions) {
+    $id = [string]$item.subscriptionId
+    New-AzManagementGroupSubscription -GroupName $managementGroup -SubscriptionId $id -WhatIf
+    Write-Output "----------------"
 }
+
