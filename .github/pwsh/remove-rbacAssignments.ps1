a0d2e698989eaedce9d027c7e5592e84fad21520
Mantas <your.email@example.com>
2024-08-16 21:18:30 +0200

    remove rbac roles

--- .github/pwsh/remove-rbacAssignments.ps1
+++ .github/pwsh/remove-rbacAssignments.ps1
@@ -1,13 +1,13 @@
 param (
     [Parameter(Mandatory = $true)]
-    [string]$managementGroup
+    [string]$managementGroup = "gazelle-test"
 )
 
 $query = "authorizationresources
-| where type == 'microsoft.authorization/roleassignments'
-| where properties.scope contains '$managementGroup'
-| where properties.roleDefinitionId !contains '18d7d88d-d35e-4fb5-a5c3-7773c20a72d9'
-| project id"
+    | where type == 'microsoft.authorization/roleassignments'
+    | where properties.scope contains '$managementGroup'
+    | where properties.roleDefinitionId !contains '18d7d88d-d35e-4fb5-a5c3-7773c20a72d9'
+    | project id"
 
 $rbacAssignments = Search-AzGraph -Query $query -ManagementGroup $managementGroup
 $rbacAssignments.Count
@@ -17,7 +17,7 @@ $rbacAssignments.Count
 # | Where-Object { $_.RoleAssignmentId -like "*${$managementGroup}*" `
 #         -and $_.RoleDefinitionName -ne 'User Access Administrator' }
 
-$rbacAssignments | ForEach-Object {
-    $role = $_
-    $role | Remove-AzRoleAssignment -Force
-}
+# $rbacAssignments | ForEach-Object {
+#     $role = $_
+#     $role | Remove-AzRoleAssignment -Force
+# }
