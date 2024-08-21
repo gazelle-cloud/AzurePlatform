43f3fd645fc2d459c9904aeb31aac413e87b7d28
Mantas <your.email@example.com>
2024-08-21 20:35:22 +0200

    remove mgmt level stack

---
+++ .github/destoryPlatform/remove-deploymentStacks.ps1
@@ -0,0 +1,22 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$managementSubscscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]$topLevelManagementGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]$workloadIdentityStackName
+)
+
+
+Select-AzSubscription -subscriptionId '${{ env.managementSubscscriptionId }}'
+
+$subscriptionStacks =  Get-AzSubscriptionDeploymentStack 
+Write-Output "deployment stacks to be deleted:"
+$subscriptionStacks.name
+$subscriptionStacks | foreach-object -ThrottleLimit 5 -Parallel {
+    Remove-AzSubscriptionDeploymentStack -ResourceId $_.id -ActionOnUnmanage DeleteAll -Force
+}
+
+
+$IdentityStack = Get-AzManagementGroupDeploymentStack -ManagementGroupId $topLevelManagementGroupName -Name $workloadIdentityStackName
+Remove-AzManagementGroupDeploymentStack -ResourceId $IdentityStack.id -ActionOnUnmanage DeleteAll -Force
