cd03290ee6ccc5516ab0dc99cd62a7be23927dd6
Mantas <your.email@example.com>
2024-09-04 20:45:25 +0200

    cleanup subscription dpeloyments

--- .github/utils/remove-deploymentHistory.ps1
+++ .github/utils/remove-deploymentHistory.ps1
@@ -7,14 +7,14 @@ param (
 
 
 $deploymants = Get-AzManagementGroupDeployment -ManagementGroupId $topLevelManagementGroupName
-$deploymants.Count
+write-output "management group deployment: $($deploymants.Count)"
 $deploymants | foreach-object -ThrottleLimit 25 -Parallel {
     Remove-AzManagementGroupDeployment -Id $_.Id -verbose
 }
 
 
-$deploymants = Get-AzSubscriptionDeployment -Id 'ca29cb06-f5f6-4116-9061-493426c4d70e'
-$deploymants.Count
+$deploymants = Get-AzSubscriptionDeployment -Id $managementSubscscriptionId
+write-output "subscription deployment: $($deploymants.Count)"
 $deploymants | foreach-object -ThrottleLimit 25 -Parallel {
     Remove-AzSubscriptionDeployment -id $_.id -verbose -WhatIf
 }
